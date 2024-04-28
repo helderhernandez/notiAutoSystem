76b70b9905432be8e6262b52df8d06117285294c
MacOsX <macosx@iMac-de-MacOsX.local>
2024-04-28 10:37:54 -0700

    First commit

---
+++ notiAutoSystem/notiAutoSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  notiAutoSystemApp.swift
+//  notiAutoSystem
+//
+//  Created by MacOsX on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct notiAutoSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
